a94573219c533c024a773da8bdcd3c5de5cbc6a4
Ship <sh1ptoast@duck.com>
2026-01-19 18:40:49 -0500

    Update guess_and_win_a_FAKE_cc_number.cpp
    updated parentheses to be output easier

--- guess_and_win_a_FAKE_cc_number.cpp
+++ guess_and_win_a_FAKE_cc_number.cpp
@@ -6,7 +6,6 @@
 #include <chrono>
 using namespace std;
 
-char par = '"';
 double guess;
 
 string generatecc() { // generates the cc number
@@ -19,7 +18,7 @@ string generatecc() { // generates the cc number
 
 int main() {
     cout << "Welcome to the guessing game! Guess a number and win a credit card number!" <<endl;
-    cout << "Sh1ptoast INC is required by law to state that the " << par << "credit card number" << par;
+    cout << "Sh1ptoast INC is required by law to state that the \"credit card number\"";
     cout << " is randomly generated and not real." << endl;
     srand(static_cast<unsigned int>(time(0))); // creates randomness
     cout << "Your guess: ";
@@ -29,7 +28,7 @@ int main() {
         cout << "WRONG!!! No credit card for you! What kind of idiot can't guess a number..." << endl;
     } else { // if you guess a number
         cout << "Correct! Your credit card number is: " << generatecc() << endl;
-        cout << "Remember that Sh1ptoast INC is required by law to state that the " << par << "credit card number" << par;
+        cout << "Remember that Sh1ptoast INC is required by law to state that the \"credit card number\"";
         cout << " is randomly generated and not real." << endl;
     }
     return 0;
